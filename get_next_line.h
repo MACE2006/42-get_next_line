a0f1b08e608e93653d50c8cee95f3cc03d3b23ba
Callum Vorley <cvorley@c1r5s2.42london.com>
2025-10-28 10:24:26 +0000

    Added foundation files

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cvorley <cvorley@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 18:38:54 by cvorley           #+#    #+#             */
+/*   Updated: 2025/10/27 19:00:24 by cvorley          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+//headers
+#include <stdlib.h>
+#include <stdio.h>
+
+
+//functions
+char *get_next_line(int fd);
+
+#endif
---
+++ test.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cvorley <cvorley@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 18:44:23 by cvorley           #+#    #+#             */
+/*   Updated: 2025/10/27 19:02:49 by cvorley          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+int	main(void)
+{
+	int		fd;
+	char *line;
+	fd = open("test.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Error");
+		return (1);
+	}
+	line = get_next_line(fd);
+	while (line)
+	{
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	free(line);
+	close(fd);
+	return(0);
+}
